b769f784440eb0ee98c8c10a5cbab8b3beb290a0
Akmal Muzaffarov <akmalmuzaffarovkdp@gmail.com>
2025-03-24 12:05:19 +0500

    Create intro.py

---
+++ lesson-1/homework/intro.py
@@ -0,0 +1,30 @@
+# Given a side of square. Find its perimeter and area.
+
+a = 3
+
+print(pow(a,2))
+
+# Given diameter of circle. Find its length.
+
+d = 5
+pi =  3.14
+
+print(d*pi)
+
+# Given two numbers a and b. Find their mean.
+
+a = 8
+b = 20
+
+print((a+b)/2)
+
+
+# Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 20
+b = 40
+
+print(a+b)
+print(a*b)
+print(pow(a,2)) 
+print(b**2) 
